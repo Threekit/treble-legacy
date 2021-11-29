6dcb6e3d99f285df708a488c65bd3a376f6c2d43
Amaan Saeed <asaeed@threekit.com>
2021-11-29 18:46:39 -0500

    implements eslint

---
+++ packages/create-treble-app/templates/basic/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'eslint-plugin-react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
