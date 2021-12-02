f4ac4b85844ea2d81dea56f90a092091c56f7a29
Amaan Saeed <asaeed@threekit.com>
2021-12-02 08:44:38 -0500

    [treble react] updates component PortalToElement

--- packages/treble-react/src/components/PortalToElement/index.tsx
+++ packages/treble-react/src/components/PortalToElement/index.tsx
@@ -3,22 +3,19 @@ import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
 interface IPortalToElementProps {
-  elementId?: string;
+  to?: string;
   strict?: boolean;
 }
 
 export const PortalToElement: React.FC<IPortalToElementProps> = props => {
-  const { children, elementId, strict } = Object.assign(
-    { strict: false },
-    props
-  );
+  const { children, to, strict } = Object.assign({ strict: false }, props);
 
-  if (!elementId) {
+  if (!to) {
     if (strict) return null;
     return <>{children}</>;
   }
 
-  const htmlEl = document.getElementById(elementId);
+  const htmlEl = document.getElementById(to);
 
   if (!htmlEl) {
     if (strict) return null;
@@ -32,7 +29,7 @@ PortalToElement.propTypes = {
   /**
    * The id of the HTML element you want the content to be rendered in
    */
-  elementId: PropTypes.string,
+  to: PropTypes.string,
   /**
    * Defines the behaviour if the HTML element is not found. If strict is
    * set to `true` the content will not render, if set to `false` the
