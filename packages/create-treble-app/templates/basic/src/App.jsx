8be1abfb1c38f67770c1664643a98bd77f85289c
Amaan Saeed <asaeed@threekit.com>
2021-12-10 13:32:15 -0500

    [CRA] defaults to TrebleApp workflow

--- packages/create-treble-app/templates/basic/src/App.jsx
+++ packages/create-treble-app/templates/basic/src/App.jsx
@@ -1,26 +1,7 @@
-import {
-  ThreekitProvider,
-  Player,
-  PortalToElement,
-  FlatForm,
-  Share,
-} from '@threekit-tools/treble';
+import { TrebleApp } from '@threekit-tools/treble';
 
 const App = () => {
-  return (
-    <ThreekitProvider>
-      <div className="tk-treble-player">
-        <Player>
-          <Player.TopRightWidgets>
-            <Share />
-          </Player.TopRightWidgets>
-        </Player>
-      </div>
-      <PortalToElement to="tk-treble-form" strict={true}>
-        <FlatForm />
-      </PortalToElement>
-    </ThreekitProvider>
-  );
+  return <TrebleApp />;
 };
 
 export default App;
