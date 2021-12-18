6d9317eac576da9a95d4cb427386ce8a081856c0
Amaan Saeed <asaeed@threekit.com>
2021-12-18 17:56:26 -0500

    updates portal to player workflow

--- packages/treble-react/src/hooks/usePlayerPortal/index.tsx
+++ packages/treble-react/src/hooks/usePlayerPortal/index.tsx
@@ -1,24 +1,62 @@
+import { useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { getPlayerElementId } from '../../store/threekit';
+import { TK_PLAYER_LOADER_DIV } from '../../constants';
 
-const usePlayerPortal = () => {
+interface IHtmlElements {
+  player: HTMLElement | null;
+  playerLoader: HTMLElement | null;
+  playerWrapper: HTMLElement | null;
+}
+
+type UsePlayerPortal =
+  | [undefined, undefined]
+  | [(toEl: string) => void, () => void];
+
+const usePlayerPortal = (): UsePlayerPortal => {
+  const htmlElements = useRef<IHtmlElements>({
+    player: null,
+    playerLoader: null,
+    playerWrapper: null,
+  });
   const playerElementId = useSelector(getPlayerElementId);
 
-  if (!playerElementId) return undefined;
+  if (!playerElementId) return [undefined, undefined];
 
   const portalPlayerTo = (toEl: string) => {
     if (!toEl) return;
 
-    const playerEl = document.getElementById(playerElementId);
-    const playerWrapperEl = document.getElementById(toEl);
+    if (!htmlElements.current.player)
+      htmlElements.current.player = document.getElementById(playerElementId);
+    htmlElements.current.playerWrapper = document.getElementById(toEl);
 
-    if (!playerEl) throw new Error('Move from element not found');
-    if (!playerWrapperEl) throw new Error('Move To element not found');
+    if (!htmlElements.current.player)
+      throw new Error('Move from element not found');
+    if (!htmlElements.current.playerWrapper)
+      throw new Error('Move To element not found');
 
-    playerWrapperEl.appendChild(playerEl);
+    htmlElements.current.playerWrapper.appendChild(htmlElements.current.player);
   };
 
-  return portalPlayerTo;
+  const returnPlayer = () => {
+    TK_PLAYER_LOADER_DIV;
+
+    if (!htmlElements.current.player)
+      htmlElements.current.player = document.getElementById(playerElementId);
+
+    if (!htmlElements.current.playerLoader)
+      htmlElements.current.playerLoader =
+        document.getElementById(TK_PLAYER_LOADER_DIV);
+
+    if (!htmlElements.current.player)
+      throw new Error('Move from element not found');
+    if (!htmlElements.current.playerLoader)
+      throw new Error('Player Loader element not found');
+
+    htmlElements.current.playerLoader.appendChild(htmlElements.current.player);
+  };
+
+  return [portalPlayerTo, returnPlayer];
 };
 
 export default usePlayerPortal;
