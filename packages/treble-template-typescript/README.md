e89ac2fa72cfe7ab49dcb9526ae13bb66052cc9d
Amaan Saeed <asaeed@threekit.com>
2022-02-18 12:41:41 -0500

    [treble-template-ts] implements typescript template

---
+++ packages/treble-template-typescript/README.md
@@ -0,0 +1,5 @@
+# treble-template-typescript
+
+This is the official typescript template for [Create Treble App](https://treble.threekit.com).
+
+To use the typescript template, you will need to specify the `--template=typescript` flag when creating your Treble App.
---
+++ packages/treble-template-typescript/template/.treble/player.config.js
@@ -0,0 +1,12 @@
+export default {
+  showConfigurator: false,
+  initialConfiguration: undefined,
+  showLoadingThumbnail: false,
+  showLoadingProgress: true,
+  // onLoadingProgress: undefined,
+  showAR: true,
+  showShare: false,
+  // locale: undefined,
+  allowMobileVerticalOrbit: false,
+  // publishStage: undefined,
+};
